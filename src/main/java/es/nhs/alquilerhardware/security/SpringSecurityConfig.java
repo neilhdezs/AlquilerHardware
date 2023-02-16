380fe4f83c196d4d89a3bd5dbfc4f1bc98ca9d36
neilhdezs <hernandezsalvadorneil@gmail.com>
2023-02-16 18:37:33 +0100

    security completed

--- src/main/java/es/nhs/alquilerhardware/security/SpringSecurityConfig.java
+++ src/main/java/es/nhs/alquilerhardware/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package es.nhs.alquilerhardware.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,10 +19,13 @@ public class SpringSecurityConfig
     {
         //http://acodigo.blogspot.com/2017/05/tutorial-spring-security-basico.html
         http
-                .authorizeRequests()
-                .requestMatchers("/","/aula/", "/cancelarAula/",  "/cancelarOrdenadores/", "/cancelarTablets/" , "/confirmar/",  "/pcs/", "/reservas/",  "/reservas/aulas/", "/reservas/pcs/",  "/reservas/tablets/", "/tablets/").hasRole("ADMIN")
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/css/**", "/js/**", "/","/aulas/", "/aula/", "/cancelarAula/",  "/cancelarOrdenadores/", "/cancelarTablets/" , "/confirmar/",  "/pcs/", "/reservas/",  "/reservas/aulas/", "/reservas/pcs/",  "/reservas/tablets/", "/tablets/", "/eliminarReserva/").hasAnyRole("ADMIN", "USER_CARRITO_1PLANTA", "USER_CARRITO_2PLANTA")
                 .and().formLogin()
-                .and().logout();
+                .and().logout()
+                .and().httpBasic();
+
         return http.build();
     }
 
